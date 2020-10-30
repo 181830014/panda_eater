fc9a11a2e9f4af873bbce2441880ee32ba1fc1bd
Ocelot079 <1485475010@qq.com>
2020-10-30 17:51:28 +0800

    wordcloud

--- cloud.py
+++ cloud.py
@@ -1,15 +1,29 @@
 from wordcloud import WordCloud
-
+from textblob import TextBlob
 
 if __name__ == '__main__':
-    f = open('data.txt', 'r', encoding='gb18030')
-    plain_text = f.read()
-    text = ''
-    for line in plain_text.splitlines():
-        if ('[closed]' not in line) and ('[duplicate]' not in line):
-            text += line
-            print(line)
 
-    cloud = WordCloud().generate(text)
+    keywords = ''
+    for idx in range(1,21):
+        print(idx)
+        text = ''
+        filepath = './results/data' + str(idx) + '.txt'
+        f = open(filepath, 'r', encoding='utf8').read()
+        plain_text = f.lower()
+        for line in plain_text.splitlines():
+            if 'answer' in line:
+                continue
+            if '[closed]' in line or '[duplicate]' in line:
+                continue
+            # words = line.split(' ')
+            # if(len(words) == 0):
+            #     continue
+            text += line
+        blob = TextBlob(text)
+        print(len(blob.noun_phrases))
+        for word in blob.noun_phrases:
+            keywords += word + ' ' 
+    
+    cloud = WordCloud().generate(keywords)
     image = cloud.to_image()
     image.show()
